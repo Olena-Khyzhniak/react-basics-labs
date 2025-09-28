bfb95abfaa4a403036f078a09358d2caaa70e1da
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-09-28 19:23:58 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Task = (props) => {
+    let priorityClass = "";
+    if (props.priority === 'High') 
+        priorityClass = 'high';
+    if (props.priority === 'Medium') 
+        priorityClass = 'medium';
+    if (props.priority === 'Low') 
+        priorityClass = 'low';
+    
     
     return (
          <div className="card">
@@ -8,6 +16,7 @@ const Task = (props) => {
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
             <p className="description">{props.description}</p>
+            <p className={`priority ${priorityClass}`} > priority: {props.priority}</p>
         </div>
     )
 }
